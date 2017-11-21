331fe8e5044f8f81b0ee6a294acdcfd7f8cd0153
Peter Širka <petersirka@gmail.com>
2017-11-21 10:21:31 +0100

    Improve model.

--- jcomponent/spa/models/users.js
+++ jcomponent/spa/models/users.js
@@ -1,9 +1,20 @@
 NEWSCHEMA('User').make(function(schema) {
 
 	schema.setQuery(function($) {
+
+		var options = $.query ? $.query : $.options;
 		var builder = NOSQL('users').find();
-		builder.take(100);
-		builder.callback($.callback);
+
+		builder.paginate(options.page || 0, options.limit || 30, 70);
+
+		builder.callback(function(err, response, count) {
+			var output = {};
+			output.page = 1;
+			output.limit = builder.$take;
+			output.items = response;
+			output.count = count;
+			$.callback(output);
+		});
 	});
 
 });
